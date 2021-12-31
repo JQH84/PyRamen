feee3762a91b17da4c7729d7888c29848ef3bced
Jihad Al-Hussain <jehad.alhussain@gmail.com>
2021-12-30 18:58:35 -0700

    deleted jupyter notebook and changed output file to txt

--- PyRamen.py
+++ PyRamen.py
@@ -6,11 +6,11 @@ from tqdm import tqdm  # used to create a progress bar while doing the for loop
 menu_filepath = Path("Resources/menu_data.csv")
 sales_filepath = Path("Resources/sales_data.csv")
 
-# @TODO: Initialize list objects to hold our menu and sales data
+# Initialize list objects to hold our menu and sales data
 menu = []
 sales = []
 
-# @TODO: Read in the menu data into the menu list
+# Read in the menu data into the menu list
 with open(menu_filepath, "r") as csv_file:
     menu_data = csv.reader(csv_file, delimiter=",")
     menu_data_header = next(csv_file)
@@ -18,7 +18,7 @@ with open(menu_filepath, "r") as csv_file:
     for row in menu_data:
         menu.append(row)
 
-# @TODO: Read in the sales data into the menu list
+# Read in the sales data into the menu list
 with open(sales_filepath, "r") as csv_file:
     sales_data = csv.reader(csv_file, delimiter=",")
     sales_data_header = next(csv_file)
@@ -26,18 +26,18 @@ with open(sales_filepath, "r") as csv_file:
     for row in sales_data:
         sales.append(row)
 
-# @TODO: Initialize dict object to hold our key-value pairs of items and metrics
+# Initialize dict object to hold our key-value pairs of items and metrics
 result = {}
 
 # Initialize a row counter variable
 row_count = 0
 
-# @TODO: Loop over every row in the sales list object
+# Loop over every row in the sales list object
 
 for row in tqdm(sales):
 
     # Line_Item_ID,Date,Credit_Card_Number,Quantity,Menu_Item
-    # @TODO: Initialize sales data variables
+    # Initialize sales data variables
     quantity = int(row[3])
     menu_item = row[4]
 
@@ -47,19 +47,19 @@ for row in tqdm(sales):
     if menu_item not in result.keys():
         result[menu_item] = {"count": 0, "revenue": 0, "cogs": 0, "profit": 0}
 
-    # @TODO: For every row in our sales data, loop over the menu records to determine a match
+    # For every row in our sales data, loop over the menu records to determine a match
     for row in menu:
         # Item,Category,Description,Price,Cost
-        # @TODO: Initialize menu data variables
+        # Initialize menu data variables
         item = row[0]
         price = float(row[3])
         cost = float(row[4])
 
-        # @TODO: Calculate profit of each item in the menu data
+        # Calculate profit of each item in the menu data
         profit = price - cost
-        # @TODO: If the item value in our sales data is equal to the any of the items in the menu, then begin tracking metrics for that item
+        # If the item value in our sales data is equal to the any of the items in the menu, then begin tracking metrics for that item
         if menu_item == item:
-            # @TODO: Print out matching menu data
+            # Print out matching menu data
             # save a dict of the matched items to be printed in summary
             matched_items = {
                 "item": item,
@@ -67,29 +67,30 @@ for row in tqdm(sales):
                 "cost": cost,
                 "profit": profit,
             }
-            # print(f"Found a match /n Item: {item} /n Price: {price} /n Cost: {cost} /n Profit: {profit}")
-            # @TODO: Cumulatively add up the metrics for each item key
+            print(
+                f"Found a match /n Item: {item} /n Price: {price} /n Cost: {cost} /n Profit: {profit}"
+            )
+            # Cumulatively add up the metrics for each item key
             result[menu_item]["count"] += quantity
             result[menu_item]["revenue"] += price * quantity
             result[menu_item]["cogs"] += cost * quantity
             result[menu_item]["profit"] += profit * quantity
 
-        # @TODO: Else, the sales item does not equal any fo the item in the menu data, therefore no match
+        # Else, the sales item does not equal any fo the item in the menu data, therefore no match
         # else:
         # print(f"{item} does not match any item in the menu ")
 
-    # @TODO: Increment the row counter by 1
+    # Increment the row counter by 1
     row_count += 1
 
-# @TODO: Print total number of records in sales data
+# Print total number of records in sales data
 # printing the summary after the progress bar completes
 print(f"the total number of records are {row_count}")
-for i in matched_items:
-    print(f"the matched items are below: \n {matched_items}")
 
-# @TODO: Write out report to a text file (won't appear on the command line output)
 
-results_path = Path("results/output.csv")
+# Write out report to a text file (won't appear on the command line output)
+
+results_path = Path("results/output.txt")
 with open(results_path, "w") as csvout:
     for key, value in result.items():
         row = "%s %s \n" % (key, value)
